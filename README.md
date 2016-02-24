d6d4cac8cc48caa0c26fca663dbc03a881c6b8d9
Vadorequest <ambroise.dhenain@gmail.com>
2016-02-25 00:30:09 +0100

    Ajout du Spinner. (ajout de modifications précédentes non commitées...) ~20mn

--- README.md
+++ README.md
@@ -17,15 +17,16 @@ The goal is to make the given application, following the given specifications wi
     - [x] city name
     - [x] area for today's result
     - [x] area for next three days results
-    - [x] styling (bootstrap) (~~forgot~~ 20mn)
-- [x] Load JSON data to fill select input. (~~forgot~~ 30mn) *[Issues with Angular load dependecies]*
-- [ ] Call API endpoint to get the data for today. (30mn)
-- [ ] Call API endpoint to get the data for the next three days. (15mn)
-- [ ] Display API results. (30mn)
-    - [ ] weather icon depending on the result
-    - [ ] min/max temperature in Celsius degrees
-- [ ] Display results for the first city of the list by default. (10mn)
-- [ ] Display pre-loader when a city is selected while we're fetching the data from the API endpoint. (30mn)
+    - [x] styling (bootstrap) (~~forgotten~~ 20mn)
+- [x] Load JSON data to fill select input. (~~forgotten~~ 30mn) *[Issues with Angular load dependecies]*
+- [x] Call API endpoint to get the data for today. (30mn)
+- [x] Call API endpoint to get the data for the next three days. (15mn)
+- [x] Display API results. (30mn)
+    - [x] weather icon depending on the result
+    - [x] min/max temperature in Celsius degrees
+- [x] Display results for the first city of the list by default. (10mn)
+- [x] Display loader while fetching data at startup. (20mn)
+- [ ] Display pre-loader when a city is selected while we're fetching the data from the API endpoint. (10mn)
 
 Total estimated time: 155mn
 Total time spent: 
@@ -39,7 +40,8 @@ Total estimated time (with optional features): 205mn
 
 *Misc:*
 
-- [ ] Reading specs and project management (30mn)
+- [x] Reading specs, project management. (~~30mn~~ 45mn)
 - [x] Serve static file through node server. (~~10mn~~ 5mn)
+- [ ] Read and understand the Weather API. (~~forgotten~~ 25mn)
 
 Total estimated time (with optional features and misc): 245mn
--- app/directives/TdlWeather/TdlWeather.js
+++ app/directives/TdlWeather/TdlWeather.js
@@ -1,4 +1,4 @@
-angular.module('app').directive('tdlWeather', function(CityService){
+angular.module('app').directive('tdlWeather', function(CityService, WeatherService){
     return {
         restrict: 'E',
         replace: true,
@@ -14,14 +14,66 @@ angular.module('app').directive('tdlWeather', function(CityService){
 
         },
         link: function(scope, elem, attrs) {
+            scope.city = null;
             scope.cities = [];
+            scope.current = null;
+            scope.forecast = null;
+            scope.helper = {
+                isReady_current: false,
+                isReady_forecast: false
+            };
 
+            // Load cities from JSON to populate the <select> options.
             $.get('mocks/cities-fr.json')// TODO Use service... But data must have been fetched before.
                 .success(function(cities){
                     scope.cities = cities;
                     scope.city = scope.cities[0];
                     scope.$apply();
+
+                    _updateCurrent(scope.city);
+                    _updateForecast(scope.city);
                 });
+
+            _handleSuccessResponse = function(response, type){
+                if(response.cod.toString() === '404'){
+                    console.error(response);
+                    alert("Impossible de localiser la ville demandée.");
+                }else if(response.cod.toString() === '200'){
+                    scope[type] = response;
+                    scope.helper['isReady_' + type] = true;
+                }else{
+                    console.error(response);
+                    alert("Une erreur est survenue.");
+                }
+            };
+
+            _handleErrorResponse = function (data, httpCode) {
+                if(httpCode === 429){
+                    alert("Il y a trop d'appels HTTP à l'API. Veuillez réessayer dans quelques instants.")
+                }
+            };
+
+            _updateCurrent = function(city){
+                scope.helper.isReady_current = false;
+                WeatherService.getCurrent(city)
+                    .success(function(response){
+                        _handleSuccessResponse(response, 'current');
+                    })
+                    .error(function(data, httpCode){
+                        _handleErrorResponse(data, httpCode);
+                    });
+            };
+
+            _updateForecast = function(city){
+                scope.helper.isReady_forecast = false;
+                WeatherService.getForecast(city)
+                    .success(function(response){
+                        _handleSuccessResponse(response, 'forecast');
+                    })
+                    .error(function(data, httpCode){
+                        _handleErrorResponse(data, httpCode);
+                    });
+            };
 
             scope.onChange = function(input){
                 console.log(input)
